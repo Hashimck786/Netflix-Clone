46380afa0a5834ce6cc4a5fdc04112dd081ac9af
Hashimck786 <hsmhashim786@gmail.com>
2024-07-09 14:17:03 +0530

    added RowPost Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import NavBar from "./components/navbar/NavBar";
 import './app.css'
 import Banner from "./components/banner/Banner";
+import RowPost from "./components/rowpost/RowPost";
 
 function App() {
   return (
     <div className="App">
      <NavBar/>
      <Banner/>
+     <RowPost/>
     </div>
   );
 }
